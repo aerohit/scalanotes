0ffb0b4e46da4f53d5238d1a06744118a8f7ff92
Rohit Saxena <aerohit.saxena@gmail.com>
2014-04-28 19:14:25 +0200

    Left and right projections of Either types

--- Either.scala
+++ Either.scala
@@ -13,10 +13,25 @@ object ContentReader {
 object Eithering extends App {
   import ContentReader._
 
-  println(getContent("http://www.aerohitsaxena.com"))
-
   getContent("https://www.google.com") match {
     case Left(msg) => println(msg)
     case Right(content) => content.getLines.foreach(println)
   }
+
+  val contentR: Either[String, Iterator[String]] =
+    getContent("http://aerohitsaxena.com").right.map(_.getLines)
+
+  val moreContentR: Either[String, Iterator[String]] =
+    getContent("http://google.map").right.map(_.getLines)
+
+  val contentL: Either[Iterator[String], Source] =
+    getContent("http://aerohitsaxena.com").left.map(Iterator(_))
+
+  val moreContentL: Either[Iterator[String], Source] =
+    getContent("http://google.map").left.map(Iterator(_))
+
+  println(contentR)
+  println(moreContentR)
+  println(contentL)
+  println(moreContentL)
 }
