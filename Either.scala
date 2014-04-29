f373d7fb3070db3314c9288d115ff4dc65859daa
Rohit Saxena <aerohit.saxena@gmail.com>
2014-04-29 22:52:16 +0200

    Using Either to collection operations.

--- Either.scala
+++ Either.scala
@@ -74,4 +74,20 @@ object Eithering extends App {
   println(content)
   println(moreContent)
   println(parseURL("http"))
+
+  type Citizen = String
+  case class BlackListedResource(url: URL, visitors: Set[Citizen])
+
+  val blacklist = List(
+    BlackListedResource(new URL("https://google.com"), Set("John Doe", "Johanna Doe")),
+    BlackListedResource(new URL("http://yahoo.com"), Set.empty),
+    BlackListedResource(new URL("https://maps.google.com"), Set("John Doe")),
+    BlackListedResource(new URL("http://plus.google.com"), Set.empty)
+  )
+  val checkedblacklist: List[Either[URL, Set[Citizen]]] = blacklist.map {
+    resource =>
+      if (resource.visitors.isEmpty) Left(resource.url)
+      else Right(resource.visitors)
+  }
+  println(checkedblacklist)
 }
