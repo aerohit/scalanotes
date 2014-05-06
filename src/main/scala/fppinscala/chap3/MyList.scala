8db16874fecb8a6b14c5f31cd0692de1fee4a2f4
Rohit Saxena <aerohit.saxena@gmail.com>
2014-05-07 00:27:25 +0200

    Implemented tail and setHead for MyList

--- src/main/scala/fppinscala/chap3/MyList.scala
+++ src/main/scala/fppinscala/chap3/MyList.scala
@@ -12,9 +12,20 @@ object MyList {
 
   def product(list: MyList[Int]): Int = list match {
     case MyNil => 1
+    case MyCons(0, _) => 0
     case MyCons(head, tail) => head * product(tail)
   }
   
+  def tail[A](list: MyList[A]): MyList[A] = list match {
+    case MyNil => throw new RuntimeException
+    case MyCons(_, t) => t
+  }
+
+  def setHead[A](list: MyList[A], a: A): MyList[A] = list match {
+    case MyNil => throw new RuntimeException
+    case MyCons(_, t) => MyCons(a, t)
+  }
+
   def apply[A](elements: A*): MyList[A] =
     if (elements.isEmpty) MyNil
     else MyCons(elements.head, apply(elements.tail: _*))
--- src/test/scala/fppinscala/chap3/MyListTest.scala
+++ src/test/scala/fppinscala/chap3/MyListTest.scala
@@ -14,6 +14,18 @@ class MyListTest extends Specification {
       product(MyList()) mustEqual 1
       product(MyList(1, 2, 3)) mustEqual 6
     }
+
+    "be able to be tailed" in {
+      tail(MyList()) must throwA[RuntimeException]
+      tail(MyList(1)) mustEqual MyList()
+      tail(MyList(7, 1, 2, 4)) mustEqual MyList(1, 2, 4)
+    }
+
+    "should support setHead" in {
+      setHead(MyList(), 5) must throwA[RuntimeException]
+      setHead(MyList(1), 3) mustEqual MyList(3)
+      setHead(MyList(1, 2, 4, 6), 4) mustEqual MyList(4, 2, 4, 6)
+    }
   }
 }
 
