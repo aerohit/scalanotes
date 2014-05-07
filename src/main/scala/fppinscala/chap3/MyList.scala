8de97d6beb3e2caee87cb9c477c94835f712412a
Rohit Saxena <aerohit.saxena@gmail.com>
2014-05-07 17:12:04 +0200

    Implemented drop, dropWhile, append, init, foldLeft and foldRight. Also implemented sum, product and length in terms of foldLeft and foldRight.

--- src/main/scala/fppinscala/chap3/MyList.scala
+++ src/main/scala/fppinscala/chap3/MyList.scala
@@ -10,20 +10,76 @@ object MyList {
     case MyCons(head, tail) => head + sum(tail)
   }
 
+  def sumFR(list: MyList[Int]): Int = foldRight(list, 0)(_ + _)
+
+  def sumFL(list: MyList[Int]): Int = foldLeft(list, 0)(_ + _)
+
   def product(list: MyList[Int]): Int = list match {
     case MyNil => 1
     case MyCons(0, _) => 0
     case MyCons(head, tail) => head * product(tail)
   }
   
+  def productFR(list: MyList[Int]): Int = foldRight(list, 1)(_ * _)
+  
+  def productFL(list: MyList[Int]): Int = foldLeft(list, 1)(_ * _)
+
+  // Ex 2
   def tail[A](list: MyList[A]): MyList[A] = list match {
     case MyNil => throw new RuntimeException
     case MyCons(_, t) => t
   }
 
+  // Ex 3
   def setHead[A](list: MyList[A], a: A): MyList[A] = list match {
     case MyNil => throw new RuntimeException
     case MyCons(_, t) => MyCons(a, t)
+  }
+
+  // Ex 4
+  def drop[A](list: MyList[A], n: Int): MyList[A] =
+    if (n <= 0) list
+    else list match {
+      case MyNil => throw new RuntimeException
+      case MyCons(_, t) => drop(t, n - 1)
+    }
+
+  // Ex 5
+  def dropWhile[A](list: MyList[A], p: A => Boolean): MyList[A] = list match {
+    case MyNil => MyNil
+    case l @ MyCons(h, t) => if (p(h)) dropWhile(t, p) else l
+  }
+
+  def dropWhileCurried[A](list: MyList[A])(p: A => Boolean): MyList[A] = list match {
+    case MyNil => MyNil
+    case l @ MyCons(h, t) => if (p(h)) dropWhile(t, p) else l
+  }
+
+  def append[A](list1: MyList[A], list2: MyList[A]): MyList[A] = list1 match {
+    case MyNil => list2
+    case MyCons(h, t) => MyCons(h, append(t, list2))
+  }
+
+  // Ex 6
+  def init[A](list: MyList[A]): MyList[A] = list match {
+    case MyNil => throw new RuntimeException
+    case MyCons(h, MyNil) => MyNil
+    case MyCons(h, t) => MyCons(h, init(t))
+  }
+
+  def foldRight[A, B](as: MyList[A], z: B)(f: (A, B) => B): B = as match {
+    case MyNil => z
+    case MyCons(h, t) => f(h, foldRight(t, z)(f))
+  }
+
+  def lengthFR[A](list: MyList[A]): Int = foldRight(list, 0)((_, b) => b + 1)
+
+  def lengthFL[A](list: MyList[A]): Int = foldLeft(list, 0)((b, _) => b + 1)
+
+  def foldLeft[A, B](as: MyList[A], z: B)(f: (B, A) => B): B = as match {
+    case MyNil => z
+    case MyCons(h, t) => foldLeft(t, f(z, h))(f)
+    //case MyCons(h, t) => f(foldLeft(t, z)(f), h)
   }
 
   def apply[A](elements: A*): MyList[A] =
--- src/test/scala/fppinscala/chap3/MyListTest.scala
+++ src/test/scala/fppinscala/chap3/MyListTest.scala
@@ -15,16 +15,108 @@ class MyListTest extends Specification {
       product(MyList(1, 2, 3)) mustEqual 6
     }
 
+    // Ex 2
     "be able to be tailed" in {
       tail(MyList()) must throwA[RuntimeException]
       tail(MyList(1)) mustEqual MyList()
       tail(MyList(7, 1, 2, 4)) mustEqual MyList(1, 2, 4)
     }
 
-    "should support setHead" in {
+    // Ex 3
+    "support setHead" in {
       setHead(MyList(), 5) must throwA[RuntimeException]
       setHead(MyList(1), 3) mustEqual MyList(3)
       setHead(MyList(1, 2, 4, 6), 4) mustEqual MyList(4, 2, 4, 6)
+    }
+
+    // Ex 4
+    "drop elements" in {
+      drop(MyList(), 1) must throwA[RuntimeException]
+      drop(MyList(1, 2, 4, 6), 6) must throwA[RuntimeException]
+      drop(MyList(), 0) mustEqual MyList()
+      drop(MyList(1, 2, 4, 3), 3) mustEqual MyList(3)
+      drop(MyList(1, 2, 4, 6), 4) mustEqual MyList()
+    }
+
+    // Ex 5
+    "drop while predicate is true" in {
+      dropWhile(MyList(), (x: Int) => x > 4) mustEqual MyList()
+      dropWhile(MyList(8, 5, 1, 2, 4, 6, 9), (x: Int) => x > 4) mustEqual MyList(1, 2, 4, 6, 9)
+    }
+
+    "support drop while predicates without type specification with a curried definition" in {
+      dropWhileCurried(MyList[Int]())(x => x > 4) mustEqual MyList()
+      dropWhileCurried(MyList(8, 5, 1, 2, 4, 6, 9))(x => x > 4) mustEqual MyList(1, 2, 4, 6, 9)
+    }
+
+    "append another list" in {
+      append(MyList(), MyList()) mustEqual MyList()
+      append(MyList(1), MyList()) mustEqual MyList(1)
+      append(MyList(), MyList(1)) mustEqual MyList(1)
+      append(MyList(7, 8, 9), MyList(3, 2, 1)) mustEqual MyList(7, 8, 9, 3, 2, 1)
+    }
+
+    "return init" in {
+      init(MyList()) must throwA[RuntimeException]
+      init(MyList(1)) mustEqual MyList()
+      init(MyList(1, 3, 5, 8)) mustEqual MyList(1, 3, 5)
+    }
+
+    "foldRight" in {
+      foldRight(MyList[Int](), 0)(_ + _) mustEqual 0
+      foldRight(MyList(1), 0)(_ + _) mustEqual 1
+      foldRight(MyList(1, 4, 8), 0)(_ + _) mustEqual 13
+      foldRight(MyList[Int](), 1)(_ * _) mustEqual 1
+      foldRight(MyList(7), 1)(_ * _) mustEqual 7
+      foldRight(MyList(3, 5, 2), 1)(_ * _) mustEqual 30
+    }
+
+    "foldRight with MyNil and MyCons(...)" in {
+      foldRight(MyList(3, 5, 2), MyList[Int]())(MyCons(_, _)) mustEqual MyList(3, 5, 2)
+    }
+
+    "compute sum of integerst using foldRight" in {
+      sumFR(MyList()) mustEqual 0
+      sumFR(MyList(1, 2, 3)) mustEqual 6
+    }
+
+    "calculate product of integers using foldRight" in {
+      productFR(MyList()) mustEqual 1
+      productFR(MyList(1, 2, 3)) mustEqual 6
+    }
+
+    // Ex 9
+    "compute length using foldRight" in {
+      lengthFR(MyList[Int]()) mustEqual 0
+      lengthFR(MyList(1)) mustEqual 1
+      lengthFR(MyList(0, 0, 0)) mustEqual 3
+    }
+
+    // Ex 10
+    "foldLeft" in {
+      foldLeft(MyList[Int](), 0)(_ + _) mustEqual 0
+      foldLeft(MyList(1), 0)(_ + _) mustEqual 1
+      foldLeft(MyList(1, 4, 8), 0)(_ + _) mustEqual 13
+      foldLeft(MyList[Int](), 1)(_ * _) mustEqual 1
+      foldLeft(MyList(7), 1)(_ * _) mustEqual 7
+      foldLeft(MyList(3, 5, 2), 1)(_ * _) mustEqual 30
+    }
+
+    // Ex 11
+    "compute length using foldLeft" in {
+      lengthFL(MyList[Int]()) mustEqual 0
+      lengthFL(MyList(1)) mustEqual 1
+      lengthFL(MyList(0, 0, 0)) mustEqual 3
+    }
+
+    "compute sum of integerst using foldRight" in {
+      sumFL(MyList()) mustEqual 0
+      sumFL(MyList(1, 2, 3)) mustEqual 6
+    }
+
+    "calculate product of integers using foldRight" in {
+      productFL(MyList()) mustEqual 1
+      productFL(MyList(1, 2, 3)) mustEqual 6
     }
   }
 }
